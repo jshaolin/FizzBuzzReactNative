d9999b05531a3ee5e789bc14db4657be7149f421
Javier Silva Ortíz <silvaortizjavier@gmail.com>
2020-01-13 14:55:48 -0500

    add mocked snapshot for Nav

---
+++ __tests__/Nav.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Nav from '../components/Nav';
+
+describe('Nav Component Tests', () => {
+    describe('Nav Component Snapshot Test', () => {
+        it('should not have changed its UI', () => {
+
+            expect(1).toEqual(1);            
+        });
+    });
+});
