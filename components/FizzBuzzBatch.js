e371eb1d426039545716cc78eacb1f94c3c4424b
Javier Silva Ortíz <silvaortizjavier@gmail.com>
2020-01-13 14:51:49 -0500

    refactored FizzBuzzBatch to pass tests

--- components/FizzBuzzBatch.js
+++ components/FizzBuzzBatch.js
@@ -31,7 +31,8 @@ export default class FizzBuzzBatch extends Component {
                         this.setState({rangeString: value});
                     }}
                     onSubmitEditing = {() => {
-                        
+                        let fromTo = getValidRange(this.state.rangeString);
+                        this.batchFizzBuzz(fromTo.from, fromTo.to);
                     }}
                 >
                 </TextInput>
@@ -49,6 +50,111 @@ export default class FizzBuzzBatch extends Component {
         );
     }
 
+    batchFizzBuzz(from, to) {
+        if (isNaN(from) || isNaN(to)) {
+            return null;
+        }
+
+        let inCache = this.cache.get({from: from, to: to});
+        if (inCache != undefined) {
+            return inCache;
+        }
+
+        let request = new XMLHttpRequest();
+        request.onreadystatechange = (e) => {
+            if (request.readyState !== 4) {
+                return;
+            }
+            
+            if (request.status === 200) {
+                console.log('Request done');
+                ToastAndroid.showWithGravity('Fetching done', ToastAndroid.SHORT, ToastAndroid.BOTTOM);
+    
+                let result = JSON.parse(request.response);
+                this.addToCache({from: from, to: to}, result);
+                this.setState({batch: result});
+            } 
+            else {
+                console.log('NETWORK ERROR');
+                ToastAndroid.showWithGravity('Fetching failed', ToastAndroid.SHORT, ToastAndroid.BOTTOM);
+            }
+        };
+        request.open('GET', 'https://fizzbuzzbatch.glitch.me/batch/' + from + ' ' + to);
+        request.send();
+    
+        
+    }
+
+    addToCache(range, batch) {
+        if (this.isCacheFull()) {
+            //Cache is full, drop the oldest entry.
+            this.deleteOldestEntry();
+        }
+    
+        //Cache has space, add it.
+        this.cache.set(range, batch);
+        this.cacheAge.push(range);
+    }
+
+    isCacheFull() {
+        return this.cache.size === this.props.cacheLimit;
+    }
+
+    getOldestEntry() {
+        return this.cacheAge.shift();
+    }
+
+    deleteOldestEntry() {
+        let toDelete = this.getOldestEntry();
+        return this.cache.delete(toDelete);
+    }
+
+    isIntegerRange(rangeString) {
+        return rangeString.trim().match(/-?[0-9]+\s-?[0-9]+/) != null;
+    }
+    
+    isOrderedRange(from, to) {
+        if (from > to) {
+            return false;
+        }
+    
+        return true;
+    }
+    
+    toIntEndpoints(from, to) {
+        return [parseInt(from), parseInt(to)];
+    }
+    
+    getEndpoints(rangeString) {
+        return rangeString.split(' ');
+    }
+    
+    getValidRange(rangeString) {
+        if (this.isIntegerRange(rangeString)) {
+            let fromTo = this.getEndpoints(rangeString);
+            
+            let [from, to] = this.toIntEndpoints(fromTo[0], fromTo[1]);
+                    
+            if (this.isOrderedRange(from, to)) {
+                return {from: from, to: to}
+            }
+        }
+    
+        return {from: NaN, to: NaN}
+    }
+
+    filterFizzBuzz(queryString, data) {
+        let filter = data;
+    
+        if (queryString.length > 0) {
+            filter = filter.filter((item) => {
+                return item.fizzbuzz === queryString;
+            });
+        }
+    
+        return filter;
+    }
+}
 
 const styles = StyleSheet.create({
     main: {
